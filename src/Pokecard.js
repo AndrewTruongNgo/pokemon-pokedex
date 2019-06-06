5af5aa7f929fcab0fc4c7336184814638d9a55db
Andrew Ngo <andrewtruongngo@gmail.com>
2019-06-05 18:45:38 -0700

    Uses higher resolution images

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './Pokecard.css';
 
+const padToThree = (number) => {
+	if (number <= 999) {
+		let num = `00${number}`;
+		return num.slice(-3);
+	}
+};
+
+const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`;
+
 class Pokecard extends React.Component {
 	render() {
-		const { id, name, type, base_experience } = this.props;
-		const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+		const { id, name, type, exp } = this.props;
+		const imgSrc = `${POKE_API}${padToThree(id)}.png`;
 		return (
 			<div className="Pokecard">
 				<h1 className="Pokecard-title">{name}</h1>
-				<img src={POKE_API} alt={name} />
+				<img src={imgSrc} alt={name} />
 				<div className="Pokecard-data">Type: {type}</div>
-				<div className="Pokecard-data">EXP: {base_experience}</div>
+				<div className="Pokecard-data">EXP: {exp}</div>
 			</div>
 		);
 	}
